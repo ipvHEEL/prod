15f620ebcd5645b8f87233996cf7b3f668aca7fa
Andrey Rogov <118340357+ipvHEEL@users.noreply.github.com>
2025-01-19 15:00:13 +0300

    Update adm.py

--- adm.py
+++ adm.py
@@ -1,11 +1,9 @@
-from app import app, db, Service  # Импортируем приложение и нужные элементы
+from app import app, db, Service  
 
-# Открываем контекст приложения
+
 with app.app_context():
-    # Создание всех таблиц в базе данных
     db.create_all()
 
-    # Добавление нового объекта в базу данных
     new_service = Service(name='Ремонт фасадов', description='Ремонт фасадов зданий.')
     db.session.add(new_service)
     db.session.commit()
