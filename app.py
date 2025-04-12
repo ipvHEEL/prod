13fa308b18290d3bfeb6b2454382aea896528073
Andrey Rogov <118340357+ipvHEEL@users.noreply.github.com>
2025-04-12 15:12:10 +0300

    Update app.py

--- app.py
+++ app.py
@@ -23,6 +23,13 @@ app.secret_key = 'key'
 
 # Инициализация базы данных
 db = SQLAlchemy(app)
+class Service(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    image_url = db.Column(db.String(300), nullable=True)
+    def __repr__(self):
+        return f'<Service {self.name}>'
 
 # Модель данных для заявок
 class Application(db.Model):
@@ -36,28 +43,31 @@ class Application(db.Model):
 # Инициализация админки
 admin = Admin(app, name='Админ панель', template_mode='bootstrap3')
 admin.add_view(ModelView(Application, db.session))
+admin.add_view(ModelView(Service, db.session))
+
 
 # Главная страница с формой для заявки
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    services = Service.query.all()  # Получаем все услуги из БД
+
     if request.method == 'POST':
         try:
             fio = request.form['fio']
             number = request.form['number']
             logging.debug(f"Received form data: FIO={fio}, Number={number}")
 
-            # Сохранение заявки в базе данных
             new_application = Application(FIO=fio, Number=number)
             db.session.add(new_application)
             db.session.commit()
 
             logging.info("Application successfully submitted!")
-            return redirect('/')  # Перенаправление после успешной отправки
+            return redirect('/')
         except Exception as e:
             logging.error(f"Error processing form submission: {e}", exc_info=True)
-            return render_template('index.html', error="Произошла ошибка. Попробуйте снова.")
+            return render_template('index.html', error="Произошла ошибка. Попробуйте снова.", services=services)
 
-    return render_template('index.html')
+    return render_template('index.html', services=services)
 
 
 
